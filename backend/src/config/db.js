55fa79b32e6301b6a17d12cd7d6ba1ae34a717d4
sandesh4747 <sangitthapa328@gmail.com>
2025-12-08 21:11:28 +0545

    changes

---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV.DB_URL);
+    console.log(`✅Connected to MONGODB:${conn.connection.host}`);
+  } catch (error) {
+    console.log("MONGODB connection error");
+    process.exit(1);
+  }
+};
