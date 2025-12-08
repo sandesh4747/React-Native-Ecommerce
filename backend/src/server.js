bcb04884befc27ddb26c6d4b0fd93d6724533cb3
sandesh4747 <sangitthapa328@gmail.com>
2025-12-08 21:10:44 +0545

    db config & tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,25 @@
 import express from "express";
+import path from "path";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
+const __dirname = path.resolve();
+app.use(clerkMiddleware());
+app.use(express.json());
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
 });
 
-app.listen(3000, () => console.log("Server is up and running"));
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+app.listen(ENV.PORT, () => {
+  console.log("Server is up and running");
+  connectDB();
+});
