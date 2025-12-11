a37999b973b522c11e23b6db341f81ba37b65fe5
sandesh4747 <sangitthapa328@gmail.com>
2025-12-11 19:47:20 +0545

    changes

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
