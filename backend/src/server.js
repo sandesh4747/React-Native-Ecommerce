b5a479446fdc0e4b7c829912cf3a2780ef6eab89
sandesh4747 <sangitthapa328@gmail.com>
2025-12-11 19:37:53 +0545

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,16 +17,20 @@ app.get("/api/health", (req, res) => {
 });
 
 // make our app ready for deployment
-if (ENV.NODE_ENV === "production") {
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../admin/dist")));
+//   app.get("/{*any}", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+//   });
+// }
+
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../admin", "dist", "index.html"))
+  );
 }
-// app.listen(ENV.PORT, () => {
-//   console.log("Server is up and running");
-//   connectDB();
-// });
+
 const startServer = async () => {
   await connectDB();
   app.listen(ENV.PORT, () => {
